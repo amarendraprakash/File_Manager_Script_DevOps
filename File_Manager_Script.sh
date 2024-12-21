addb3ac4b0e18423651cf038702c1d89105895f1
Amarendra Prakash <100341879+amarendraprakash@users.noreply.github.com>
2024-12-21 22:07:19 +0530

    Update File_Manager_Script.sh

--- File_Manager_Script.sh
+++ File_Manager_Script.sh
@@ -1,5 +1,6 @@
-show_menu()
-{
+#!/bin/bash
+
+show_menu() {
  echo "================================================================="
  echo "                     File Manager Script                         "
  echo "================================================================="
@@ -12,63 +13,61 @@ show_menu()
  echo "================================================================="
 }
 
-create_file()
-{
- read -p "Enter the file name to create: " file_name
- if [[ -e $file_name ]]; then
- echo "File already exists"
+create_file() {
+ read -r -p "Enter the file name to create: " file_name
+ if [[ -e "$file_name" ]]; then
+   echo "File already exists"
  else
- touch $file_name
- echo "File $file_name created successfully"
+   touch "$file_name"
+   echo "File $file_name created successfully"
  fi
 }
 
-create_directory()
-{
- read -p "Enter the directory name to create: " directory_name
- if [[ -d $directory_name ]]; then
- echo "Directory already exists"
+create_directory() {
+ read -r -p "Enter the directory name to create: " directory_name
+ if [[ -d "$directory_name" ]]; then
+   echo "Directory already exists"
  else
- mkdir $directory_name
- echo "Directory $directory_name created successfully"
+   mkdir "$directory_name"
+   echo "Directory $directory_name created successfully"
  fi
 }
-list_item()
-{
+
+list_item() {
  echo "Listing files and directories in pwd $(pwd): "
  ls -l
 }
-delete_file()
-{
- read -p "Enter the file name you want to delete: " file_name
- if [ -e $file_name ]; then
- rm "$file_name"
- echo "File $file_name deleted successfully"
+
+delete_file() {
+ read -r -p "Enter the file name you want to delete: " file_name
+ if [[ -e "$file_name" ]]; then
+   rm "$file_name"
+   echo "File $file_name deleted successfully"
  else
- echo "File does not exist"
+   echo "File does not exist"
  fi
 }
-delete_directory()
-{
- read -p "Enter the directory name you want to delete: " directory_name
- if [[ -d $directory_name ]]; then
- rm -r "$directory_name"
- echo "Directory '$directory_name' deleted successfully"
+
+delete_directory() {
+ read -r -p "Enter the directory name you want to delete: " directory_name
+ if [[ -d "$directory_name" ]]; then
+   rm -r "$directory_name"
+   echo "Directory '$directory_name' deleted successfully"
  else
- echo "Directory does not exist"
+   echo "Directory does not exist"
  fi
 }
 
 while true; do
-show_menu
-read -p "Enter your choice [1-6]: " choice
-case $choice in
-1) create_file;;
-2) create_directory;;
-3) list_item;;
-4) delete_file;;
-5) delete_directory;;
-6) echo "Exiting script !! Goodbye"; exit 0;;
-*) echo "Invalid choice please eneter a number between 1 to 6";;
-esac
+  show_menu
+  read -r -p "Enter your choice [1-6]: " choice
+  case $choice in
+    1) create_file;;
+    2) create_directory;;
+    3) list_item;;
+    4) delete_file;;
+    5) delete_directory;;
+    6) echo "Exiting script !! Goodbye"; exit 0;;
+    *) echo "Invalid choice please enter a number between 1 to 6";;
+  esac
 done
